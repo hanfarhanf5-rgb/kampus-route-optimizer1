6e3f5b4ec45f539b8fa2eda8da8e947812125239
hanfarhanf5-rgb <hanfarhanf5@gmail.com>
2026-06-09 07:40:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -143,5 +143,8 @@ def export():
     return send_file(mem, mimetype="text/csv", as_attachment=True, download_name="hasil_simulasi.csv")
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+import os
+
+if __name__=="__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
